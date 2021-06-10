bd96224724ee0e080f2563088a031f5d66f18e06
vegaal17 <vegaal17@uia.no>
2021-06-10 18:43:37 +0200

    changes for the better

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -6,12 +6,16 @@ import EpisodePopup from '../EpisodePopup'
 import Filter from '../Filter'
 import LoadingIndicator from '../LoadingIndicator'
 import useMarkers from '../../hooks/useMarkers'
+import SwipeableBottomSheet from 'react-swipeable-bottom-sheet'
+import { useMediaQuery } from 'react-responsive'
 
 const App: React.FC = () => {
     const [filteredEpisodes, setFilteredEpisodes] = useState<Episode[]>([])
     const [filters, setFilters] = useState<string[]>([])
 
     const { episodes, isFetching, error } = useMarkers()
+
+    const isMobile = useMediaQuery({ query: '(max-width: 1024px)' })
 
     useEffect(() => {
         if (episodes)
@@ -25,33 +29,36 @@ const App: React.FC = () => {
                 <LoadingIndicator />
             ) : (
                 <>
-                    {!error ? (
+                    {!error && !isMobile && (
                         <div className="grid">
                             <Filter filters={filters} onChange={setFilters} />
-                            <MapContainer
-                                zoom={5}
-                                center={[65.376, 12.524]}
-                                style={{ height: '100%' }}
-                            >
-                                <TileLayer
-                                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-                                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-                                />
-
-                                {filteredEpisodes
-                                    ? filteredEpisodes.map((e) => (
-                                          <React.Fragment key={e.key}>
-                                              <Marker position={[e.lat, e.lng]}>
-                                                  <EpisodePopup
-                                                      programId={e.key}
-                                                  />
-                                              </Marker>
-                                          </React.Fragment>
-                                      ))
-                                    : null}
-                            </MapContainer>
+                            <DistankbMap
+                                filteredEpisodes={filteredEpisodes}
+                            ></DistankbMap>
                         </div>
-                    ) : (
+                    )}
+
+                    {!error && isMobile && (
+                        <>
+                            <DistankbMap
+                                filteredEpisodes={filteredEpisodes}
+                            ></DistankbMap>
+                            <div className="z-index">
+                                <SwipeableBottomSheet
+                                    defaultOpen
+                                    overlay={false}
+                                    overflowHeight={64}
+                                >
+                                    <Filter
+                                        filters={filters}
+                                        onChange={setFilters}
+                                    />
+                                </SwipeableBottomSheet>
+                            </div>
+                        </>
+                    )}
+
+                    {error && (
                         <div className="gradient-background fill-viewport flex">
                             <h1 className="no-margin">
                                 Tjenesta er diverre utilgjengeleg
@@ -65,3 +72,32 @@ const App: React.FC = () => {
 }
 
 export default App
+
+type DistankbMapProps = {
+    filteredEpisodes: Episode[]
+}
+
+const DistankbMap: React.FC<DistankbMapProps> = (props) => {
+    return (
+        <MapContainer
+            zoom={5}
+            center={[65.376, 12.524]}
+            style={{ height: '100%' }}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+                url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+            />
+
+            {props.filteredEpisodes
+                ? props.filteredEpisodes.map((e) => (
+                      <React.Fragment key={e.key}>
+                          <Marker position={[e.lat, e.lng]}>
+                              <EpisodePopup programId={e.key} />
+                          </Marker>
+                      </React.Fragment>
+                  ))
+                : null}
+        </MapContainer>
+    )
+}
